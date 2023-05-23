e21a87885e04e4016eab3e7a2911ad3d88bcbf81
rizilmaharjan <rizilmaharjan@gmail.com>
2023-05-23 08:57:18 +0545

    typo fixed

--- abstractwebsite/src/components/Footer.jsx
+++ abstractwebsite/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <>
-      <div className="bg-black lg:grid lg:grid-cols-2 lg:gap-x-10 lg:py-24">
-        <div className="grid grid-cols-2 gap-y-8 py-4 px-4 lg:w-[50rem] lg:grid lg:grid-cols-4 lg:mx-24 lg:gap-x-44  lg:h-fit ">
+      <div className="bg-black lg:py-24 lg:grid lg:grid-cols-2">
+        <div className="grid grid-cols-2 gap-y-8 py-4 px-4 lg:grid lg:grid-cols-4 lg:gap-x-28 lg:w-full">
           <div className="text-white">
             <div>
               <h1 className="text-2xl lg:font-semibold">Abstract</h1>
@@ -49,11 +49,14 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="lg:mx-72 lg:mt-40 lg:h-fit lg:w-7/12">
-          <h1 className="text-white text-2xl">© Copyright 2023</h1>
+        <div className="px-4 lg:flex lg:flex-col lg:justify-end lg:items-center lg:w-9/12 lg:mx-auto">
+          <div>
+
+          <h1 className="text-white text-2xl py-2">© Copyright 2023</h1>
           <p className="text-white">© Copyright 2023</p>
           <p className="text-white">Abstract Studio Design, Inc.</p>
           <p className="text-white">All rights reserved</p>
+          </div>
         </div>
       </div>
     </>
