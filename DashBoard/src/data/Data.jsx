4bee8185124a02261f8cd92c1f1b777c0e885cc1
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-15 20:46:49 +0545

    Data file added

--- DashBoard/src/data/Data.jsx
+++ DashBoard/src/data/Data.jsx
@@ -1,32 +1,202 @@
+import classNames from "classnames";
 export const Data = [
-    {
-      name: 'January',
-      Total: 1200
-    },
-    {
-      name: 'February',
-      Total: 2100
+  {
+    name: "January",
+    Total: 1200,
+  },
+  {
+    name: "February",
+    Total: 2100,
+  },
+  {
+    name: "March",
+    Total: 800,
+  },
+  {
+    name: "April",
+    Total: 1600,
+  },
+  {
+    name: "May",
+    Total: 900,
+  },
+  {
+    name: "June",
+    Total: 1700,
+  },
+];
 
+export const userColumns = [
+  { field: "id", headerName: "ID", width: 70 },
+  {
+    field: "user",
+    headerName: "User",
+    width: 230,
+    renderCell: (params) => {
+      return (
+        <div className="cellWithImg">
+          <img className="cellImg" src={params.row.img} alt="avatar" />
+          {params.row.username}
+        </div>
+      );
     },
-    {
-        name: 'March',
-        Total: 800
-  
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 230,
+  },
+
+  {
+    field: "age",
+    headerName: "Age",
+    width: 100,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 160,
+    renderCell: (params) => {
+      return (
+        <div className={`cellWithStatus ${params.row.status}`}>
+          {params.row.status}
+        </div>
+      );
     },
-    {
-        name: 'April',
-        Total: 1600
-  
+  },
+];
+
+//temporary data
+export const userRows = [
+  {
+    id: 1,
+    username: "Snow",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    status: "active",
+    email: "1snow@gmail.com",
+    age: 35,
+  },
+  {
+    id: 2,
+    username: "Jamie Lannister",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "2snow@gmail.com",
+    status: "passive",
+    age: 42,
+  },
+  {
+    id: 3,
+    username: "Lannister",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "3snow@gmail.com",
+    status: "pending",
+    age: 45,
+  },
+  {
+    id: 4,
+    username: "Stark",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "4snow@gmail.com",
+    status: "active",
+    age: 16,
+  },
+  {
+    id: 5,
+    username: "Targaryen",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "5snow@gmail.com",
+    status: "passive",
+    age: 22,
+  },
+  {
+    id: 6,
+    username: "Melisandre",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "6snow@gmail.com",
+    status: "active",
+    age: 15,
+  },
+  {
+    id: 7,
+    username: "Clifford",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "7snow@gmail.com",
+    status: "passive",
+    age: 44,
+  },
+  {
+    id: 8,
+    username: "Frances",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "8snow@gmail.com",
+    status: "active",
+    age: 36,
+  },
+  {
+    id: 9,
+    username: "Roxie",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "snow@gmail.com",
+    status: "pending",
+    age: 65,
+  },
+  {
+    id: 10,
+    username: "Roxie",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "snow@gmail.com",
+    status: "active",
+    age: 65,
+  },
+];
+
+export const column = [
+  { field: "id", headerName: "ID", width: 70 },
+  {
+    field: "user",
+    headerName: "User",
+    width: 230,
+    renderCell: (params) => {
+      return (
+        <div className="flex items-center gap-5">
+          <img
+            className="w-10 rounded-3xl h-10"
+            src={params.row.img}
+            alt="avatar"
+          />
+          {params.row.username}
+        </div>
+      );
     },
-    {
-        name: 'May',
-        Total: 900
-  
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 230,
+  },
+
+  {
+    field: "age",
+    headerName: "Age",
+    width: 100,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 160,
+    renderCell: (params) => {
+      return (
+        <div className={classNames("py-1 px-3 rounded-md",{
+          "bg-green-200" : params.row.status === "active",
+          "text-green-600" : params.row.status === "active",
+          "bg-red-200" : params.row.status === "passive",
+          "text-red-500" : params.row.status === "passive",
+          "bg-yellow-300" : params.row.status === "pending",
+          "text-yellow-600" : params.row.status === "pending",
+        })}>
+          {params.row.status}
+        </div>
+      );
     },
-    {
-        name: 'June',
-        Total: 1700
-  
-    },
-    
-  ];
+  },
+];
