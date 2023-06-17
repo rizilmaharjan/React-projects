989f21dbc88a69f9c1fa5b7d9cabc4f5bd558d72
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-17 13:47:36 +0545

    updated New page

--- DashBoard/src/pages/new/New.jsx
+++ DashBoard/src/pages/new/New.jsx
@@ -1,9 +1,12 @@
 import Sidebar from "../../components/Sidebar";
 import Navbar from "../../components/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-import { productInputs } from "../../data/FormSource";
+import { Form } from "../../components/Form";
+import Buttonn from "../../components/Button";
+import { useState } from "react";
 
-const New = () => {
+const New = ({inputs,title}) => {
+  const [file, setFile] = useState("")
   return (
     <>
       <div className="flex">
@@ -12,31 +15,37 @@ const New = () => {
           <Navbar />
           <div className="px-4">
             <div className="bg-white drop-shadow-xl py-4 px-3 text-2xl font-semibold mt-4 ">
-              <h1 className="text-[#908E9B] opacity-40">Add New User</h1>
+              <h1 className="text-[#908E9B] opacity-40">{title}</h1>
             </div>
-            <div className="flex">
+            <div className="flex mt-10 bg-white drop-shadow-xl py-4">
               <div className="flex-[1]">
                 <img
-                  className="rounded-full"
-                  src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                  className="rounded-full mx-auto"
+                  src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"}
                   alt="upload"
-                  width={150}
-                  height={150}
+                  width={130}
+                  height={130}
                 />
               </div>
               <div className="flex-[2]">
-                <form className="flex">
-                  <label htmlFor="file">
+                <form className="grid grid-cols-2 gap-y-8 place-items-center">
+                  <div>
+
+                  <label className="ml-[-6.5rem]" htmlFor="file">
                     Image: <DriveFolderUploadOutlinedIcon />
                   </label>
-                  {productInputs.map((val) => {
+                  <input className="hidden cursor-pointer" id="file" type="file" onChange={e=>setFile(e.target.files[0])} />
+                  </div>
+                  {inputs.map((val) => {
                     return (
-                      <div className="flex flex-col my-2 gap-y-2">
-                        <input className="border border-t-0 border-r-0 border-l-0 outline-none " type={val.type} placeholder={val.placeholder} />
-                      </div>
+                     <Form val={val} key={val.id} />
                     );
                   })}
                 </form>
+                <div className="flex justify-center">
+                 <Buttonn value={"Send"} />
+
+                </div>
               </div>
             </div>
           </div>
