ada5a290fdee0845bb7af9115075b57d1fbc2de5
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-17 13:46:48 +0545

    Dashboard updated

--- DashBoard/src/main.jsx
+++ DashBoard/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ColorContext } from "./context/ColorContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ColorContext>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ColorContext>
   </React.StrictMode>
 );
