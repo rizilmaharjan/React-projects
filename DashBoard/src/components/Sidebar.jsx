f9a2835920d00e5f58addb026baddf28f4d3f665
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-17 13:45:57 +0545

    sidebar updated

--- DashBoard/src/components/Sidebar.jsx
+++ DashBoard/src/components/Sidebar.jsx
@@ -12,13 +12,22 @@ import {
   LogoutOutlinedIcon,
   EditNotificationsOutlinedIcon,
 } from "../utils/Icons";
+import { Link } from "react-router-dom";
+import { myCustomContext } from "../context/ColorContext";
 
 const Sidebar = () => {
+  const {darkMode, setDarkMode} = myCustomContext();
+
+const handleClick = () =>{
+  setDarkMode(!darkMode)
+}
   return (
     <>
       <div className="flex flex-col items-center py-4 border-r border-black w-1/5 ">
         <div className="flex justify-center">
+          <Link to="/">
           <span className="text-purple-600 font-semibold text-2xl">Admin</span>
+          </Link>
         </div>
         <hr className="border-black w-full my-4" />
         <div className="w-full">
@@ -29,14 +38,18 @@ const Sidebar = () => {
               <DashboardIcon className="icon" /> <span className="span">Dashboard</span>
             </li>
             <p className="text-xs font-bold text-[#999] mt-4 mb-1">LISTS</p>
+            <Link to={"/users"}>
             <li className="flex items-center p-2 cursor-pointer hover:bg-[#ece7ff]">
              
               <PersonOutlineOutlinedIcon className="icon" /> <span className="span">Users</span>
             </li>
+            </Link>
+            <Link to={"/products"}>
             <li className="flex items-center p-2 cursor-pointer hover:bg-[#ece7ff]">
              
               <CategoryOutlinedIcon className="icon" /> <span className="span">Products</span>
             </li>
+            </Link>
             <li className="flex items-center p-2 cursor-pointer hover:bg-[#ece7ff]">
              
               <BorderColorOutlinedIcon className="icon" /> <span className="span">Orders</span>
@@ -81,8 +94,8 @@ const Sidebar = () => {
           </ul>
         </div>
         <div className="flex mt-6 gap-4 w-full pl-5">
-          <div className="w-10 h-10 bg-white border-2 border-black rounded-lg cursor-pointer"></div>
-          <div className="w-10 h-10 bg-black border-2 border-black rounded-lg cursor-pointer"></div>
+          <div onClick={handleClick} className="w-10 h-10 bg-white border-2 border-black rounded-lg cursor-pointer"></div>
+          <div onClick={handleClick} className="w-10 h-10 bg-black border-2 border-black rounded-lg cursor-pointer"></div>
         </div>
       </div>
     </>
