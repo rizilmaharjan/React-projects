97c1d58e096820a2fb05a9098344fa299f0d3a12
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-17 13:45:12 +0545

    Datatable updated

--- DashBoard/src/components/DataTable.jsx
+++ DashBoard/src/components/DataTable.jsx
@@ -1,29 +1,41 @@
-import { DataGrid} from '@mui/x-data-grid';
-import { userRows, column } from '../data/Data';
+import { DataGrid } from "@mui/x-data-grid";
+import { userRows, column } from "../data/Data";
+import { Link } from "react-router-dom";
 
-
-
-const DataTable = () => {
-  const actionColumn = [{field: "action", headerName:"Action", width: 200, renderCell:()=>{
-    return(
-      <div className='flex items-center gap-5'>
-        <div className='border-2 py-0.5 px-2 rounded-md text-purple-700 cursor-pointer'>
-          View
+const DataTable = ({url}) => {
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="flex items-center gap-5">
+            <div className="border-2 py-0.5 px-2 rounded-md text-purple-700 cursor-pointer">
+              <Link to={`${url}/test`}>
+                <span className="cursor-pointer"> View </span>
+              </Link>
+            </div>
+            <div className="border-2 py-0.5 px-2 rounded-md text-red-500 cursor-pointer">
+              Delete
+            </div>
           </div>
-        <div className='border-2 py-0.5 px-2 rounded-md text-red-500 cursor-pointer'>Delete</div>
-      </div>
-    )
-  }}]
+        );
+      },
+    },
+  ];
   return (
     <>
+      <Link to={`${url}/new`}>
+        <span> Add New </span>
+      </Link>
       <DataGrid
-
         rows={userRows}
         columns={column.concat(actionColumn)}
         checkboxSelection
       />
     </>
-  )
-}
+  );
+};
 
-export default DataTable
+export default DataTable;
