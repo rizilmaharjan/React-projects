05bed11c32ba54dfaff644abfb269653ce73a762
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-20 22:31:19 +0545

    added loader

---
+++ DashBoard/src/components/Loader.jsx
@@ -0,0 +1,12 @@
+const Loader = () => {
+  return (
+    <>
+      <div class="flex justify-center items-center h-screen">
+        <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-gray-900"></div>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
+
