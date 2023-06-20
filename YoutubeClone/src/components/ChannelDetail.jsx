4c717b50f50b71c1072cab8150fe623ac096df0e
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-20 22:26:06 +0545

    react-query

--- YoutubeClone/src/components/ChannelDetail.jsx
+++ YoutubeClone/src/components/ChannelDetail.jsx
@@ -1,47 +1,51 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 import Videos from "./Videos";
 import ChannelCard from "./ChannelCard";
-import { fetchFromAPI } from "../utils/fetchFromApi";
+import useFetchFromAPI from "../utils/fetchFromApi";
 const ChannelDetail = () => {
-  const [channelDetail, setChannelDetail] = useState(null)
-  // console.log(channelDetail)
-  const [videos, setVidoes] = useState([])
-    const {id} = useParams()
-    console.log("this is id" + id)
-    useEffect(()=>{
-      const fetchResults = async()=>{
-        const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
-        setChannelDetail(data?.items[0]);
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+  const { data: channelData } = useFetchFromAPI(
+    `channels?part=snippet&id=${id}`
+  );
+  const { data: videosData } = useFetchFromAPI(
+    `search?channelId=${id}&part=snippet%2Cid&order=date`
+  );
 
-        const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`)
-        setVidoes(videosData?.items)
-      }
-     fetchResults();
-    },[id])
-    return (
-      <>
-        <Box minHeight="95vh">
-          <Box>
-            <div style={{
-              background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+  useEffect(() => {
+    if (channelData) {
+      setChannelDetail(channelData?.items[0]);
+    }
+
+    if (videosData) {
+      setVideos(videosData?.items);
+    }
+  }, [channelData, videosData]);
+
+  return (
+    <>
+      <Box minHeight="95vh">
+        <Box>
+          <div
+            style={{
+              background:
+                "linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)",
               zIndex: 10,
-              height: "300px"
-            }} />
-            <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
-          </Box>
-          <Box display="flex" p="2">
-            <Box sx={{mr:{sm:"100px"}}} />
-              <Videos videos={videos} />
-
-
-          </Box>
-
+              height: "300px",
+            }}
+          />
+          <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
         </Box>
-      </>
-    )
-  }
-  
-  export default ChannelDetail
-  
+        <Box display="flex" p="2">
+          <Box sx={{ mr: { sm: "100px" } }} />
+          <Videos videos={videos} />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default ChannelDetail;
