7700ccfefa68d653f1e7784f3bd4fa3d048f5f0f
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-20 22:31:00 +0545

    react query

--- movie/src/main.jsx
+++ movie/src/main.jsx
@@ -5,10 +5,20 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Context from "./context/Context";
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+
   <Context>
     <BrowserRouter>
       <App />
     </BrowserRouter>
     </Context>
+  </QueryClientProvider>
 );
