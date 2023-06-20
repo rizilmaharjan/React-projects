5d35900ae8f1cb90e0d24bbf40dfa01d395c0ae8
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-20 22:25:41 +0545

    react query

--- YoutubeClone/src/App.jsx
+++ YoutubeClone/src/App.jsx
@@ -1,22 +1,36 @@
-import {Route, Routes} from 'react-router-dom';
-import { Box } from '@mui/material';
-import {Navbar,VideoDetail,ChannelDetail,SearchFeed,Feed} from './components'
+import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { Box } from "@mui/material";
+import { Navbar } from "./components";
+import Loader from "./components/Loader";
+const Feed = lazy(() => import("./components/Feed"));
+const VideoDetail = lazy(() => import("./components/VideoDetail"));
+const ChannelDetail = lazy(() => import("./components/ChannelDetail"));
+const SearchFeed = lazy(() => import("./components/SearchFeed"));
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
+
+const queryClient = new QueryClient();
 const App = () => {
   return (
     <>
-      <Box sx={{backgroundColor:'#000'}}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Feed />} />
-          <Route path="/video/:id" element={<VideoDetail />} />
-          <Route path="/channel/:id" element={<ChannelDetail />} />
-          <Route path="/search/:searchTerm" element={<SearchFeed />} />
-        </Routes>
-      </Box>
-
-   
+      <QueryClientProvider client={queryClient}>
+        <Box sx={{ backgroundColor: "#000" }}>
+          <Navbar />
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Feed />} />
+              <Route path="/video/:id" element={<VideoDetail />} />
+              <Route path="/channel/:id" element={<ChannelDetail />} />
+              <Route path="/search/:searchTerm" element={<SearchFeed />} />
+            </Routes>
+          </Suspense>
+        </Box>
+      </QueryClientProvider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
