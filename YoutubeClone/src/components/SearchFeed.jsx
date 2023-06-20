cc7f984d1f333a88e2c37ca5d9c362d71f221190
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-20 22:26:55 +0545

    used react query

--- YoutubeClone/src/components/SearchFeed.jsx
+++ YoutubeClone/src/components/SearchFeed.jsx
@@ -2,16 +2,28 @@ import { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import Videos from "./Videos";
-import { fetchFromAPI } from "../utils/fetchFromApi";
+import useFetchFromAPI from "../utils/fetchFromApi";
 const SearchFeed = () => {
   const [video, setVideos] = useState([]);
-  const { searchTerm} = useParams()
-  console.log("🚀 ~ file: SearchFeed.jsx:9 ~ SearchFeed ~ searchTerm:", searchTerm)
+
+  const { searchTerm } = useParams();
+  const {
+    data: searchVideo,
+    isError,
+    isLoading,
+  } = useFetchFromAPI(`search?part=snippet&q=${searchTerm}`);
+
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
-      setVideos(data.items)
-    );
-  }, [searchTerm]);
+    if (!isError && !isLoading) setVideos(searchVideo?.items);
+  }, [searchVideo]);
+
+  if (isError) {
+    return <span>Error...</span>;
+  }
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
   return (
     <>
       <Box
@@ -30,7 +42,8 @@ const SearchFeed = () => {
             color: "white",
           }}
         >
-          Search Results for: <span className="text-red-600">{searchTerm}</span> videos
+          Search Results for: <span className="text-red-600">{searchTerm}</span>{" "}
+          videos
         </Typography>
 
         <Videos videos={video} />
