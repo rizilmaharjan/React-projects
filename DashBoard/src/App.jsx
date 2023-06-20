2b062f7ae46cfd6aa6d06f4843fb291051bb02c5
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-20 22:24:31 +0545

    react-query

--- DashBoard/src/App.jsx
+++ DashBoard/src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import List from "./pages/list/List";
-import Single from "./pages/single/Single";
-import New from "./pages/new/New";
+import {lazy, Suspense} from "react"
 import { userInputs } from "./data/FormSource";
 import { productInputs } from "./data/FormSource";
 import { myCustomContext } from "./context/ColorContext";
 import classNames from "classnames";
+import Loader from "./components/Loader";
+const Login = lazy(()=>import("./pages/login/Login"))
+const Home = lazy(()=>import("./pages/home/Home"))
+const List = lazy(()=>import("./pages/list/List"))
+const Single = lazy(()=>import("./pages/single/Single"))
+const New = lazy(()=>import("./pages/new/New"))
 const App = () => {
   const {darkMode} = myCustomContext()
   return (
@@ -17,7 +19,8 @@ const App = () => {
         "bg-white" : darkMode === false
       })}>
 
-  
+      <Suspense fallback={<Loader />} >
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -32,6 +35,8 @@ const App = () => {
           <Route path="new" element={<New inputs={productInputs}  title="Add New Product" />} />
         </Route>
       </Routes>
+      </Suspense>
+
       </div>
     </>
   );
