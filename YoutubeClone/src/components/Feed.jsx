42feba48c188d23e090eac485b84fd278703a008
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-20 22:26:29 +0545

    used react query

--- YoutubeClone/src/components/Feed.jsx
+++ YoutubeClone/src/components/Feed.jsx
@@ -1,67 +1,93 @@
-import { useState, useEffect } from "react"
-import {Box, Stack, Typography} from '@mui/material'
-import SideBar from "./SideBar"
-import Videos from "./Videos"
-import { fetchFromAPI } from "../utils/fetchFromApi"
+import { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import SideBar from "./SideBar";
+import Videos from "./Videos";
+import useFetchFromAPI from "../utils/fetchFromApi";
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
-  const [video, setVideos] = useState([])
-  useEffect(()=>{
-    
-  fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-  .then((data)=>setVideos(data.items))
+  const [video, setVideos] = useState([]);
+  console.log(video)
 
-   
-  },[selectedCategory])
-    return (
-      <>
-       <Stack sx={{
-        flexDirection: {
-          sx: "column",
-          md: "row"
-        }
-       }}>
-        <Box 
+  const {
+    data: feedVideo,
+    isLoading,
+    isError,
+  } = useFetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+
+  useEffect(() => {
+    if (!isLoading && !isError) {
+      setVideos(feedVideo);
+    }
+  }, [feedVideo]);
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+  if (isError) {
+    return <span>Error...</span>;
+  }
+
+  return (
+    <>
+      <Stack
         sx={{
-          height: {
-            sx: 'auto',
-            md: '92vh'
+          flexDirection: {
+            sx: "column",
+            md: "row",
           },
-          borderRight: '1px solid #3d3d3d',
-          px: {
-            sx:0,
-            md:2
-          }
         }}
+      >
+        <Box
+          sx={{
+            height: {
+              sx: "auto",
+              md: "92vh",
+            },
+            borderRight: "1px solid #3d3d3d",
+            px: {
+              sx: 0,
+              md: 2,
+            },
+          }}
         >
-          <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+          <SideBar
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+          />
 
-          <Typography variant="body2" sx={{
-            mt: 1.5,
-            color: "#fff"
-          }}>
+          <Typography
+            variant="body2"
+            sx={{
+              mt: 1.5,
+              color: "#fff",
+            }}
+          >
             Copyright 2022 JSM Media
           </Typography>
-
         </Box>
-        <Box p={2} sx={{
-          overlowY: 'auto',
-          height: '90vh',
-          flex: 2
-        }}>
-          <Typography variant="h4" fontWeight={"bold"} mb={2} sx={{
-            color:"white"
-          }}>
+        <Box
+          p={2}
+          sx={{
+            overlowY: "auto",
+            height: "90vh",
+            flex: 2,
+          }}
+        >
+          <Typography
+            variant="h4"
+            fontWeight={"bold"}
+            mb={2}
+            sx={{
+              color: "white",
+            }}
+          >
             {selectedCategory} <span className="text-red-600">Videos</span>
           </Typography>
 
-          <Videos videos={video} />
+          {video && <Videos videos={video.items} />}
         </Box>
+      </Stack>
+    </>
+  );
+};
 
-       </Stack>
-      </>
-    )
-  }
-  
-  export default Feed
-  
+export default Feed;
