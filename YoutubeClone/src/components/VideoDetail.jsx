74f430b41a18a243d64218c7eff903d7788c4e1d
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-20 22:27:18 +0545

    used react query

--- YoutubeClone/src/components/VideoDetail.jsx
+++ YoutubeClone/src/components/VideoDetail.jsx
@@ -5,39 +5,65 @@ import { Typography, Box, Stack } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 import Videos from "./Videos";
-import { fetchFromAPI } from "../utils/fetchFromApi";
-
+import useFetchFromAPI from "../utils/fetchFromApi";
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
   const [videos, setVideos] = useState(null);
   const { id } = useParams();
 
+  const { data: videodetail } = useFetchFromAPI(
+    `videos?part=snippet,statistics&id=${id}`
+  );
+  const { data: videoData } = useFetchFromAPI(
+    `search?part=snippet&relatedToVideoId=${id}&type=video`
+  ); /* recommendation */
+
   useEffect(() => {
-    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
-      .then((data) => setVideoDetail(data.items[0]))
+    {
+      setVideoDetail(videodetail?.items[0]);
+    }
 
-    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)  /* recommendation */
-      .then((data) => setVideos(data.items))
-  }, [id]);
+    {
+      videoData && setVideos(videoData?.items);
+    }
+  }, [videodetail, videoData]);
 
-  if(!videoDetail?.snippet) return "Loading....";
+  if (!videoDetail?.snippet) return "Loading....";
 
-  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videoDetail;
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail;
 
   return (
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
             </Typography>
-            <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{ color: "#fff" }}
+              py={1}
+              px={2}
+            >
               <Link to={`/channel/${channelId}`}>
-                <Typography variant={{ sm: "subtitle1", md: 'h6' }}  color="#fff" >
+                <Typography
+                  variant={{ sm: "subtitle1", md: "h6" }}
+                  color="#fff"
+                >
                   {channelTitle}
-                  <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+                  <CheckCircleIcon
+                    sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+                  />
                 </Typography>
               </Link>
               <Stack direction="row" gap="20px" alignItems="center">
@@ -51,8 +77,13 @@ const VideoDetail = () => {
             </Stack>
           </Box>
         </Box>
-        <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
-         { videos && <Videos videos={videos} direction="column" />}
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={videos} direction="column" />
         </Box>
       </Stack>
     </Box>
