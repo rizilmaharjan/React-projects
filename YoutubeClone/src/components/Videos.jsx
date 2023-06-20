bdc2f6252e48f6b873982c2a803c988255148532
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-20 22:27:44 +0545

    added react query

--- YoutubeClone/src/components/Videos.jsx
+++ YoutubeClone/src/components/Videos.jsx
@@ -3,19 +3,19 @@ import VideoCard from "./VideoCard";
 import ChannelCard from "./ChannelCard";
 
 const Videos = ({ videos }) => {
-  console.log("🚀 ~ file: Videos.jsx:6 ~ Videos ~ videos:", videos);
+
+ if(!videos){
+  return <span style={{color:"#fff"}}>No videos found</span>
+ }
   return (
-    <>
-      <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-        {videos.map((item, idx) => (
-          <Box key={idx}>
-            {/* <p className="text-white">{item.id.videoId}</p> */}
-            {item.id.videoId && <VideoCard video={item} />}
-            {item.id.channelId && <ChannelCard channelDetail={item} />}
-          </Box>
-        ))}
-      </Stack>
-    </>
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
   );
 };
 
