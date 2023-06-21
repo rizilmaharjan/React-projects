8d7857b5d88455feb475435964f319b84dd3f762
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-21 22:35:16 +0545

    react query updated

--- movie/src/context/Context.jsx
+++ movie/src/context/Context.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState } from "react";
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const AppContext = createContext();
