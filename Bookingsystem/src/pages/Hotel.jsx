1aea2ad03bf2a82c9472319aca92764ade96ef80
rizilmaharjan <rizilmaharjan@gmail.com>
2023-07-01 20:34:18 +0545

    list page added2

--- Bookingsystem/src/pages/Hotel.jsx
+++ Bookingsystem/src/pages/Hotel.jsx
@@ -1,7 +1,9 @@
+import { Header } from "../components/Header"
 export const Hotel = () => {
   return (
     <>
-        Hotel
+      <Header />
+       
     </>
   )
 }
--- Bookingsystem/src/pages/List.jsx
+++ Bookingsystem/src/pages/List.jsx
@@ -1,7 +1,9 @@
+import { Header } from "../components/Header"
 export const List = () => {
   return (
     <>
-        List page
+      <Header type={"list"} />
+       
     </>
   )
 }
