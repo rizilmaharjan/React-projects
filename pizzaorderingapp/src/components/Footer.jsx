aed855937314c135e840cf96f396f3ca2b51ab09
rizilmaharjan <rizilmaharjan@gmail.com>
2023-07-04 21:02:50 +0545

    footer added

--- pizzaorderingapp/src/components/Footer.jsx
+++ pizzaorderingapp/src/components/Footer.jsx
@@ -2,23 +2,23 @@ export const Footer = () => {
   return (
     <>
       <div className="flex mt-10">
-        <div className="w-1/2 h-[20rem]">
+        <div className="w-1/2 h-[20rem] hidden md:block">
           <img className="h-full w-full object-cover" src="/images/bg.png" alt="background" />
         </div>
-        <div className="bg-black gap-12 grid grid-cols-3 w-2/3 py-10 px-4">
-          <div className="text-3xl capitalize text-white">
+        <div className="bg-black w-full gap-12 grid grid-cols-3 md:w-2/3 py-10 px-4">
+          <div className="text-sm md:text-3xl capitalize text-white">
             oh yes, we did. the kirtipur pizza, well baked slice of pizza.
           </div>
           <div className="text-white uppercase ">
-            <h1 className="text-yellow-600">Find our resturant</h1>
-            <p>Kirtipur, NayaBazar</p>
+            <h1 className="text-yellow-600 text-sm md:text-md">Find our resturant</h1>
+            <p className="text-sm md:text-md">Kirtipur, NayaBazar</p>
           </div>
           <div className="text-white uppercase">
-            <h1 className="text-yellow-600">working hours</h1>
-            <p>monday until firday</p>
-            <p>9:00 - 22:00</p>
-            <p>saturday-sunday</p>
-            <p>12:00-24:00</p>
+            <h1 className="text-yellow-600 text-sm md:text-md">working hours</h1>
+            <p className="text-sm md:text-md">monday until firday</p>
+            <p className="text-sm md:text-md">9:00 - 22:00</p>
+            <p className="text-sm md:text-md">saturday-sunday</p>
+            <p className="text-sm md:text-md">12:00-24:00</p>
           </div>
         </div>
       </div>
