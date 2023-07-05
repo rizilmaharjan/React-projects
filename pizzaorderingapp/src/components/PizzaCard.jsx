c7b581116f085239ace742e1e0b047e688a9b9fb
rizilmaharjan <rizilmaharjan@gmail.com>
2023-07-05 22:12:07 +0545

    pizza card added

--- pizzaorderingapp/src/components/PizzaCard.jsx
+++ pizzaorderingapp/src/components/PizzaCard.jsx
@@ -1,14 +1,16 @@
-export const PizzaCard = () => {
+export const PizzaCard = ({name,image,price}) => {
   return (
     <>
-        <div className="w-72">
-            <div className="w-1/2 mt-4">
-                <img src="images/pizza.png"  alt="image" />
-            </div>
+        <div className="w-72 h-[20rem] cursor-pointer">
+          <div className="h-60">
+            <img src={image} alt={name} className="h-full" />
+
+          </div>
+           
+           
             <div>
-                <h1 className="uppercase py-2 text-red-700 font-semibold text-xl">fiori di zucca</h1>
-                <p className="font-bold text-lg">$19.90</p>
-                <p className="py-2 text-[#908E9B]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum sunt suscipit atque. Quod atque</p>
+                <h1 className="uppercase py-2 text-red-700 font-semibold text-xl">{name}</h1>
+                <p className="font-bold text-lg">${price}</p>
             </div>
         </div>
 
