c407045c7c17a8f5959f894234ddf4d1cd59b370
rizilmaharjan <rizilmaharjan@gmail.com>
2023-07-09 16:57:39 +0545

    pizza data updated

--- pizzaorderingapp/src/data/pizzaData.js
+++ pizzaorderingapp/src/data/pizzaData.js
@@ -4,49 +4,64 @@ export const pizzaData = [
         name: "Olive Mixed pizza",
         image: "/images/olive.png",
         para: " In this classic combination the crispy pizza crust is topped with mozzarella and delicious juicy olives. It's so simple yet amazingly good!",
-        price:19.90
+        price:19.90,
+        medium: 22.25,
+        large: 30.85
+
     },
     {
         id:2,
         name: "Cheese Pizza",
         image: "/images/cheese.png",
         para: " Pizza cheese encompasses several varieties and types of cheeses and dairy products that are designed and manufactured for use specifically on pizza. ",
-        price:25.90
+        price:25.90,
+        medium: 30.25,
+        large: 35.55
     },
     {
         id:3,
         name: "Sausage Pizza",
         image: "/images/sausagePepperPizza.png",
         para: " The kind most of us know from pizza is coarsely ground pork (and sometimes beef) with fennel, also known as “mild” Italian sausage",
-        price:15.25
+        price:15.25,
+        medium: 18.15,
+        large: 22.35
     },
     {
         id:4,
         name: "Mixed pizza",
         image: "/images/mixed.png",
         para: " Mixed pizza, also known as supreme pizza, is a popular type of pizza that combines various toppings to create a flavorful and diverse eating experience. It typically includes a combination of meats, vegetables, and sometimes even seafood.",
-        price:13.90
+        price:13.90,
+        medium: 16.45,
+        large: 20.25
     },
     {
         id:5,
         name: "French Pizza",
         image: "/images/FrenchPizza.png",
         para: "  Crusty French bread is topped with garlic butter, pizza sauce, oregano, and two kinds of cheese before baking to melty perfection. Add your favorite toppings for a new pizza that your family will love!",
-        price:21.90
+        price:21.90,
+        medium: 23.35,
+        large: 28.65
     },
     {
         id:6,
         name: "Great Pizza",
         image: "/images/great.png",
         para: " Great pizza is a popular and beloved dish enjoyed by people all over the world. It originated in Naples, Italy, and has since become an international culinary icon. ",
-        price:23.90
+        price:23.90,
+        medium:27.35,
+        large: 30.25
     },
     {
         id:7,
         name: "Big Pizza",
         image: "/images/big.png",
         para: " Big pizza refers to oversized or large-sized pizzas that are typically larger than the standard pizza size. These pizzas are known for their impressive dimensions and are often shared among a group of people. ",
-        price:26
+        price:26,
+        medium: 29.33,
+        large: 33.33
     },
     {
         id: 8,
@@ -54,12 +69,8 @@ export const pizzaData = [
         image: "/images/peporoni.png",
         para: "Pepperoni pizza is a classic favorite that features slices of pepperoni, a type of cured and seasoned Italian salami, on top of melted cheese and tomato sauce.",
         price: 17.50,
+        medium: 20.55,
+        large: 26.75
     },
-    {
-        id: 9,
-        name: "Vegetarian Pizza",
-        image: "/images/vegpizza.png",
-        para: "Vegetarian pizza is a delightful option for those who prefer a meatless version. It includes a variety of vegetables such as bell peppers, onions, mushrooms, olives, and sometimes additional toppings like artichokes or spinach.",
-        price: 18.75,
-    },
+    
 ]
